a013759249d2475c6264c7696c6841304af970fc
Kluapo <kluaayushi@gmail.com>
2025-02-20 00:10:13 -0800

    CH3 L1 END

--- src/textnode.py
+++ src/textnode.py
@@ -1,20 +1,22 @@
+from htmlnode import LeafNode
 from enum import Enum
-from htmlnode import *
+
 
 class TextType(Enum):
-    TEXT= "text"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
     LINK = "link"
     IMAGE = "image"
 
+
 class TextNode:
-    def __init__(self, text, text_type, url = None):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
-        
+
     def __eq__(self, other):
         return (
             self.text_type == other.text_type
@@ -23,11 +25,12 @@ class TextNode:
         )
 
     def __repr__(self):
-        return (f"TextNode({self.text}, {self.text_type.value}, {self.url})")
-    
-def text_node_to_html_node(text_node):  
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.TEXT:
-        return LeafNode(None, value = text_node.text)
+        return LeafNode(None, text_node.text)
     if text_node.text_type == TextType.BOLD:
         return LeafNode("b", text_node.text)
     if text_node.text_type == TextType.ITALIC:
@@ -35,9 +38,7 @@ def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.CODE:
         return LeafNode("code", text_node.text)
     if text_node.text_type == TextType.LINK:
-        return LeafNode("a", text_node.text, {"href":text_node.url})
+        return LeafNode("a", text_node.text, {"href": text_node.url})
     if text_node.text_type == TextType.IMAGE:
-        return LeafNode("img", value="", props = 
-                        {"src": text_node.url,
-                         "alt": text_node.text} )
-    raise Exception("invalid text type")
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"invalid text type: {text_node.text_type}")
